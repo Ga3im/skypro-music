2d164fadca0daa4c6b79133c8986e3f616083383
Gazim <akbutin.gazim.2000@gmail.com>
2024-09-04 00:58:33 +0500

    4.5

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -32,13 +32,13 @@ export const Player = ({ thisTrack }: props) => {
     duration: 0,
   });
 
-  useEffect(()=>{
+  useEffect(() => {
     if (isPlaying) {
       audioRef.current?.play();
     } else {
       audioRef.current?.pause();
     }
-  },[isPlaying])
+  }, [isPlaying]);
 
   const audioRef = useRef<HTMLAudioElement | null>(null);
   const onPlay = () => {
@@ -64,7 +64,7 @@ export const Player = ({ thisTrack }: props) => {
   };
 
   const onRepeat = () => {
-    const audio = audioRef.current;
+    const audio: HTMLAudioElement | null = audioRef.current;
     if (repeat) {
       audio.loop = false;
     } else {
@@ -81,7 +81,7 @@ export const Player = ({ thisTrack }: props) => {
     dispatch(setPrevTrack());
   };
 
-  const progressBar = (e) => {
+  const progressBar = (e: ChangeEvent<HTMLInputElement>) => {
     if (audioRef.current) {
       audioRef.current.currentTime = Number(e.target.value);
     }
@@ -112,133 +112,137 @@ export const Player = ({ thisTrack }: props) => {
             onChange={progressBar}
           />
           <div className={s.barPlayerBlock}>
-            <div className={s.barPlayer}>
-              <div className={s.playerControls}>
-                <div onClick={PrevTrack} className={s.playerBtnPrev}>
-                  <svg className={s.playerBtnPrevSvg}>
-                    <use xlinkHref="/icon/sprite.svg#icon-prev"></use>
-                  </svg>
-                </div>
-                {isPlaying ? (
-                  <div onClick={onPlay} className={s.playerBtnPlay}>
-                    <svg className={s.playerBtnPlaySvg}>
-                      <use xlinkHref="/icon/sprite.svg#icon-pause"></use>
+            <div>
+              <div className={s.barPlayer}>
+                <div className={s.playerControls}>
+                  <div onClick={PrevTrack} className={s.playerBtnPrev}>
+                    <svg className={s.playerBtnPrevSvg}>
+                      <use xlinkHref="/icon/sprite.svg#icon-prev"></use>
                     </svg>
                   </div>
-                ) : (
-                  <div onClick={onPlay} className={s.playerBtnPlay}>
-                    <svg className={s.playerBtnPlaySvg}>
-                      <use xlinkHref="/icon/sprite.svg#icon-play"></use>
+                  {isPlaying ? (
+                    <div onClick={onPlay} className={s.playerBtnPlay}>
+                      <svg className={s.playerBtnPlaySvg}>
+                        <use xlinkHref="/icon/sprite.svg#icon-pause"></use>
+                      </svg>
+                    </div>
+                  ) : (
+                    <div onClick={onPlay} className={s.playerBtnPlay}>
+                      <svg className={s.playerBtnPlaySvg}>
+                        <use xlinkHref="/icon/sprite.svg#icon-play"></use>
+                      </svg>
+                    </div>
+                  )}
+                  <div onClick={NextTrack} className={s.playerBtnNext}>
+                    <svg className={s.playerBtnNextSvg}>
+                      <use xlinkHref="/icon/sprite.svg#icon-next"></use>
                     </svg>
                   </div>
-                )}
-                <div onClick={NextTrack} className={s.playerBtnNext}>
-                  <svg className={s.playerBtnNextSvg}>
-                    <use xlinkHref="/icon/sprite.svg#icon-next"></use>
-                  </svg>
-                </div>
 
-                {repeat ? (
-                  <div
-                    onClick={onRepeat}
-                    className={classNames(s.playerBtnRepeat, s.btnIcon)}
-                  >
-                    <svg className={s.playerBtnRepeatSvgActive}>
-                      <use xlinkHref="/icon/sprite.svg#icon-activeRepeat"></use>
-                    </svg>
-                  </div>
-                ) : (
-                  <div
-                    onClick={onRepeat}
-                    className={classNames(s.playerBtnRepeat, s.btnIcon)}
-                  >
-                    <svg className={s.playerBtnRepeatSvg}>
-                      <use xlinkHref="/icon/sprite.svg#icon-repeat"></use>
-                    </svg>
-                  </div>
-                )}
+                  {repeat ? (
+                    <div
+                      onClick={onRepeat}
+                      className={classNames(s.playerBtnRepeat, s.btnIcon)}
+                    >
+                      <svg className={s.playerBtnRepeatSvgActive}>
+                        <use xlinkHref="/icon/sprite.svg#icon-activeRepeat"></use>
+                      </svg>
+                    </div>
+                  ) : (
+                    <div
+                      onClick={onRepeat}
+                      className={classNames(s.playerBtnRepeat, s.btnIcon)}
+                    >
+                      <svg className={s.playerBtnRepeatSvg}>
+                        <use xlinkHref="/icon/sprite.svg#icon-repeat"></use>
+                      </svg>
+                    </div>
+                  )}
 
-                {isShuffle ? (
-                  <div
-                    onClick={toggleShaffle}
-                    className={classNames(s.playerBtnShuffle, s.btnIcon)}
-                  >
-                    <svg className={s.playerBtnShuffleSvg}>
-                      <use xlinkHref="/icon/sprite.svg#icon-shuffleActive"></use>
-                    </svg>
-                  </div>
-                ) : (
-                  <div
-                    onClick={toggleShaffle}
-                    className={classNames(s.playerBtnShuffle, s.btnIcon)}
-                  >
-                    <svg className={s.playerBtnShuffleSvg}>
-                      <use xlinkHref="/icon/sprite.svg#icon-shuffle"></use>
-                    </svg>
-                  </div>
-                )}
-              </div>
-
-              <div className={s.playerTrackPlay}>
-                <div className={s.trackPlayContain}>
-                  <div className={s.trackPlayImage}>
-                    <svg className={s.trackPlaySvg}>
-                      <use xlinkHref="/icon/sprite.svg#icon-note"></use>
-                    </svg>
-                  </div>
-                  <div className={s.trackPlayAuthor}>
-                    <a className={s.trackPlayAuthorLink} href="http://">
-                      {thisTrack.author}
-                    </a>
-                  </div>
-                  <div className={s.trackPlayAlbum}>
-                    <a className={s.trackPlayAlbumLink} href="http://">
-                      {thisTrack.name}
-                    </a>
-                  </div>
+                  {isShuffle ? (
+                    <div
+                      onClick={toggleShaffle}
+                      className={classNames(s.playerBtnShuffle, s.btnIcon)}
+                    >
+                      <svg className={s.playerBtnShuffleSvg}>
+                        <use xlinkHref="/icon/sprite.svg#icon-shuffleActive"></use>
+                      </svg>
+                    </div>
+                  ) : (
+                    <div
+                      onClick={toggleShaffle}
+                      className={classNames(s.playerBtnShuffle, s.btnIcon)}
+                    >
+                      <svg className={s.playerBtnShuffleSvg}>
+                        <use xlinkHref="/icon/sprite.svg#icon-shuffle"></use>
+                      </svg>
+                    </div>
+                  )}
                 </div>
 
-                <div className={s.trackPlayLikeDis}>
-                  <div className={classNames(s.trackPlayLike, s.btnIcon)}>
-                    <svg className={s.trackPlayLikeSvg}>
-                      <use xlinkHref="/icon/sprite.svg#icon-like"></use>
-                    </svg>
+                <div className={s.playerTrackPlay}>
+                  <div className={s.trackPlayContain}>
+                    <div className={s.trackPlayImage}>
+                      <svg className={s.trackPlaySvg}>
+                        <use xlinkHref="/icon/sprite.svg#icon-note"></use>
+                      </svg>
+                    </div>
+                    <div className={s.trackPlayAuthor}>
+                      <a className={s.trackPlayAuthorLink} href="http://">
+                        {thisTrack.author}
+                      </a>
+                    </div>
+                    <div className={s.trackPlayAlbum}>
+                      <a className={s.trackPlayAlbumLink} href="http://">
+                        {thisTrack.name}
+                      </a>
+                    </div>
                   </div>
-                  <div className={classNames(s.trackPlayDislike, s.btnIcon)}>
-                    <svg className={s.trackPlayDislikeSvg}>
-                      <use xlinkHref="/icon/sprite.svg#icon-dislike"></use>
-                    </svg>
+
+                  <div className={s.trackPlayLikeDis}>
+                    <div className={classNames(s.trackPlayLike, s.btnIcon)}>
+                      <svg className={s.trackPlayLikeSvg}>
+                        <use xlinkHref="/icon/sprite.svg#icon-like"></use>
+                      </svg>
+                    </div>
+                    <div className={classNames(s.trackPlayDislike, s.btnIcon)}>
+                      <svg className={s.trackPlayDislikeSvg}>
+                        <use xlinkHref="/icon/sprite.svg#icon-dislike"></use>
+                      </svg>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
+            <div className={s.timeAndVolume}>
+              <div className={s.trackTime}>
+                <p>
+                  {Math.floor(progress.currentTime / 60)}:
+                  {Math.floor(progress.currentTime % 60)
+                    .toString()
+                    .padStart(2, "0")}{" "}
+                  /
+                </p>
+                <p className={s.beforeTime}>
+                  {minutes}:{seconds.toString().padStart(2, "0")}
+                </p>
+              </div>
 
-            <div className={s.trackTime}>
-              <p>
-                {Math.floor(progress.currentTime / 60)}:
-                {Math.floor(progress.currentTime % 60)
-                  .toString()
-                  .padStart(2, "0")}
-              </p>
-              <p>
-                {minutes}:{seconds.toString().padStart(2, "0")}
-              </p>
-            </div>
-
-            <div className={s.barVolumeBlock}>
-              <div className={s.volumeContent}>
-                <div className={s.volumeImage}>
-                  <svg className={s.volumeSvg}>
-                    <use xlinkHref="/icon/sprite.svg#icon-volume"></use>
-                  </svg>
-                </div>
-                <div className={s.volumeProgress}>
-                  <input
-                    onChange={onChangeVolume}
-                    className={s.volumeProgressLine}
-                    type="range"
-                    name="range"
-                  />
+              <div className={s.barVolumeBlock}>
+                <div className={s.volumeContent}>
+                  <div className={s.volumeImage}>
+                    <svg className={s.volumeSvg}>
+                      <use xlinkHref="/icon/sprite.svg#icon-volume"></use>
+                    </svg>
+                  </div>
+                  <div className={s.volumeProgress}>
+                    <input
+                      onChange={onChangeVolume}
+                      className={s.volumeProgressLine}
+                      type="range"
+                      name="range"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
